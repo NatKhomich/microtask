ff3a0ced75598603aeffecb217100ea4c73c80fd
NatKhomich <yalapulya94@mail.ru>
2023-03-16 11:25:32 +0600

    microtask 2 week Fullinput (1 way)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React, {useState} from 'react';
 import './App.css';
 import Filter from './filter/Filter';
-import NewComponent from "./map/NewComponent";
+import NewComponent from './map/NewComponent';
 import UseState from './useState/UseState';
 import Button from './button/Button';
-import Input from './input/Input';
+import {FullInput} from './input/FullInput';
 
 
 function App() {
 
     const topCars = [ //map
-        {manufacturer:'BMW', model:'m5cs'},
-        {manufacturer:'Mercedes', model:'e63s'},
-        {manufacturer:'Audi', model:'rs6'}
+        {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'},
+        {manufacturer: 'Audi', model: 'rs6'}
     ]
 
     const Button1 = (subscriber: string) => {
@@ -20,18 +20,23 @@ function App() {
     }
 
     let [message, setMessage] = useState([
-            {message: 'message1'},
-            {message: 'message2'},
-            {message: 'message3'},
-        ])
+        {message: 'message1'},
+        {message: 'message2'},
+        {message: 'message3'},
+    ])
+
+    const addMessage = (title: string) => {
+        let newMessage = {message: title}
+        setMessage([newMessage,...message])
+    }
 
     return (
         <>
             <NewComponent topCars={topCars}/>
-            <Button name={'Кнопка'} callBack={()=>Button1('Im Natalia')} />
-            <UseState />
-            <Filter />
-            <Input />
+            <Button name={'Кнопка'} callBack={() => Button1('Im Natalia')}/>
+            <UseState/>
+            <Filter/>
+            <FullInput addMessage={addMessage}/>
 
             <div>
 
@@ -40,12 +45,11 @@ function App() {
                     <button> + </button>
                 </div>*/}
 
-
-                {message.map( (el, index) => {
+                {message.map((el, index) => {
                     return (
                         <div key={index}> {el.message} </div>
                     )
-                } )}
+                })}
             </div>
 
         </>
